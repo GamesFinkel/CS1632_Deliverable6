bfc30c5492f65fcd6c632e4cf0cd5c1f13551bb1
Emery <eac85@pitt.edu>
2018-04-12 23:29:02 -0400

    math not done

--- rpn_run.rb
+++ rpn_run.rb
@@ -1,56 +1,49 @@
 require_relative 'variables'
 # RPN decoder class
 class RPN
-
   attr_accessor :variables
-  attr_accessor :REPLmode
+  attr_accessor :repl_mode
   attr_accessor :line
 
   def initialize
     @variables = []
-    @REPLmode
-    @line = 0 
+    @repl_mode = true
+    @line = 0
   end
 
-  def start file
-    if(file == ' ')
-      puts 'No file'
-      @REPLmode = false
+  def start(file)
+    if file == ' '
+      puts 'REPL mode'
+      @repl_mode = true
       text = ' '
     else
-      @REPLmode = true
-      text = open_file file 
-      #puts text
+      @repl_mode = false
+      text = open_file file
     end
     calculations text
   end
 
-  # Tokens shall be numbers, variable names, operators, or one of the keywords QUIT, LET, or PRINT.
   def valid_token(token)
-    return false unless keyword token, false
+    return false unless keyword token
     true
   end
-  
-  def keyword token, test
-    first = token.split.first
-    if(first.casecmp('print') == 0)
-      return true if test
+
+  def keyword(token)
+    if token.split.first.casecmp('print').zero?
       print_var token
-    elsif(first.casecmp('let') == 0)
-      return true if test
+    elsif token.split.first.casecmp('let').zero?
       let_var token
-    elsif(first.casecmp('quit') == 0)
-      return true if test
+    elsif token.split.first.casecmp('quit').zero?
       quit
     else
-      false
+      raise "Keyword didn't start line #{@line}\n"
     end
-      true
+    true
   end
 
   def open_file(file)
     text = []
-    File.open(file, "r") do |f|
+    File.open(file, 'r') do |f|
       f.each_line do |line|
         text << line
       end
@@ -59,31 +52,32 @@ class RPN
   end
 
   def calculations(text)
-    token = split_line text, @line if @REPLmode
-    token = gets unless @REPLmode
-    while(valid_token token)
-      token = split_line text, @line if @REPLmode
-      token = gets unless @REPLmode
-      if(@REPLmode)
-        exit if @line == text.count
-      end
-      @line = @line + 1
+    token = split_line text, @line unless @repl_mode
+    token = gets if @repl_mode
+    while valid_token token
+      token = split_line text, @line unless @repl_mode
+      token = gets if @repl_mode
+      end_line text.count unless @repl_mode
+      @line += 1
     end
-    print "Keyword didn't start line #{@line}\n"
+  end
+
+  def end_line(count)
+    quit if @line == count
   end
 
   def split_line(text, line)
-    new_line = text[line]
+    text[line]
   end
 
   def let_var(token)
     # var[0] = keyword
     # var[1] = variable
     # var[2] = value
-    var = token.split(" ")
-    #return false unless letter var[1]
+    var = token.split(' ')
+    raise "Variable not a letter" if (letter var[1]).nil?
     raise "Not an integer at line #{@line}" unless var[2].to_i.is_a? Integer
-    variable = Variables.new var[1].downcase, var[2] 
+    variable = Variables.new var[1].downcase, var[2]
     @variables << variable
   end
 
@@ -93,18 +87,51 @@ class RPN
 
   def print_var(token)
     var = token.split(' ')
-    x = get_var(var[1]) unless var[1].to_i.is_a? Integer
+    if var.count > 3
+      math var
+    end    
+    get_var var[1] unless letter var[1].nil?
+    puts var[2] unless var[2].nil?
+    x = get_var(var[1]) if var[1].to_i.is_a? Integer
     puts x.value unless x == false
   end
 
-  def get_var variable
-    @variables.each | x |
-      if x.var == variable.downcase
-        return x
-      end
-    raise "Variable not initialized at line #{@line}" if @REPLmode
-    puts "Variable not initialized at line #{@line}" 
+  def get_var(variable)
+    @variables.each { |x| return x if x.var == variable.downcase }
+   # raise "Variable not initialized at line #{@line}" unless @repl_mode
+    puts "Variable not initialized at line #{@line}"
     false
+  end
+
+  def math(var)
+    val1 = var[1].to_i if (letter var[1]).nil?
+    val2 = var[2].to_i if (letter var[2]).nil?
+    operator = var[3] if is_operator var[3]
+    puts val1
+    puts val2
+    puts operator
+    addition var if operator.strcmp('+').zero?
+    subtraction var if operator.strcmp('-').zero?
+    multiplication var if operator.strcmp('*').zero?
+    division var if operator.strcmp('/').zero?
+  end
+
+  def addition(var)
+    
+  end
+
+  def subtraction(var)
+  end
+
+  def multiplication(var)
+  end
+
+  def division(var)
+  end
+
+  def is_operator(var)
+    ops = ['+', '-', '*', '/']
+    ops.include?(var)
   end
 
   def quit
