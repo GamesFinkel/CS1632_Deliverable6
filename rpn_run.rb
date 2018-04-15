514edadbdb4f79f4ebe02c62fd4dccf1c66e7ac5
Emery <eac85@pitt.edu>
2018-04-15 13:38:59 -0400

    stack

--- rpn_run.rb
+++ rpn_run.rb
@@ -1,5 +1,6 @@
 require_relative 'variables'
 require_relative 'repl_mode'
+require_relative 'stack'
 # RPN decoder class
 class RPN
   attr_accessor :variables
@@ -53,6 +54,7 @@ class RPN
 
   def calculations(text)
     token = split_line text, @line 
+    @line += 1
     while true
       if stack_check token
         exit(3) 
@@ -86,20 +88,29 @@ class RPN
     puts 'let'
     var = token.split(' ')
     puts var[1]
-    raise "Line #{@line + 1}: Variable not a letter" if (letter var[1]).nil?
-    raise "Line #{@line + 1}: Not an integer" unless var[2].to_i.is_a? Integer
+    raise "Line #{@line + 1}: Variable not a letter" unless letter var[1]
+    raise "Line #{@line + 1}: Not an integer" unless is_integer? var[2]
     variable = Variables.new var[1].downcase, var[2]
     @variables << variable
   end
 
   def letter(var)
     var == /[[:alpha:]]/
+    return true unless var.nil?
+    return false if var.nil?
   end
 
   def print_line(token)
     var = token.split(' ')
-    puts math token if var.count > 3
-    print_var token if var.count == 2
+    puts if is_integer? var[1]
+    puts "math: #{math token}" if var.count > 3
+    if var.count == 2
+       if is_integer? var[1]
+         puts var[1] 
+       else
+         print_var token 
+       end
+    end
   end
 
   def print_var(token)
@@ -111,43 +122,76 @@ class RPN
 
   def get_var(variable)
     @variables.each { |x| 
-      puts x.var
+      #puts x.var
       return x if x.var == variable.downcase }
-    # raise "Line #{@line}: Variable not initialized"
+    raise "Line #{@line}: Variable not initialized"
   end
 
+  # change so that input does not contain keyword or variable
   def math(token)
+    val = 0
+    stack = LinkedList::Stack.new
     var = token.split(' ')
-    var[1] = (get_var var[1]).value unless letter var[1].nil?
-    var[2] = (get_var var[2]).value unless letter var[2].nil?
-    var[1] = var[1].to_i if (letter var[1]).nil?
-    var[2] = var[2].to_i if (letter var[2]).nil?
-    operator = var[3] if operator? var[3]
-    return addition var if operator == '+'
-    return subtraction var if operator == '-'
-    return multiplication var if operator == '*'
-    return division var if operator == '/'
+    var.each {|x| 
+      # puts x
+      if is_integer? x
+        puts "#{x} is an integer on stack"
+        stack << x.to_i
+      elsif keyword? x
+        puts "keyword"
+      elsif operator?(x)
+        puts "#{x} is an operator"
+        operator = x
+        val = addition stack.pop, stack.pop if operator == '+'
+        val = subtraction stack.pop, stack.pop if operator == '-'
+        val = multiplication stack.pop, stack.pop if operator == '*'
+        val = division stack.pop, stack.pop if operator == '/'
+      elsif letter x
+        puts "letter #{x}"
+        puts "#{get_var(x).value} value"
+        stack << get_var(x.downcase).value
+      end
+      }
+    return val
+
+    #var[1] = (get_var var[1]).value unless letter var[1].nil?
+    #var[2] = (get_var var[2]).value unless letter var[2].nil?
+    #var[1] = var[1].to_i if (letter var[1]).nil?
+    #var[2] = var[2].to_i if (letter var[2]).nil?
+    #operator = var[3] if operator? var[3]
   end
 
-  def addition(var)
-    var[1].to_i + var[2].to_i
+  def addition(operand1, operand2)
+    puts "adding"
+    puts operand1
+    puts operand2
+    operand1.to_i + operand2.to_i
   end
 
-  def subtraction(var)
-    var[1].to_i - var[2].to_i
+  def subtraction(operand1, operand2)
+    operand1 - operand2
   end
 
-  def multiplication(var)
-    var[1].to_i * var[2].to_i
+  def multiplication(operand1, operand2)
+     operand1 * operand2
   end
 
-  def division(var)
-    var[1].to_i / var[2].to_i
+  def division(operand1, operand2)
+     operand1 / operand2
   end
 
   def operator?(var)
     ops = ['+', '-', '*', '/']
     ops.include?(var)
+  end
+
+  def is_integer?(x)
+    /\A[-+]?\d+\z/ === x
+  end
+
+  def keyword?(x)
+    keywordlist = ['LET', 'PRINT', 'QUIT']
+    keywordlist.include?(x)
   end
 
   def quit
